e422b19c5f3675aa63150ede4403b387cd8a7a58
Alireza Nourian <alireza.nournia@gmail.com>
2013-04-29 23:27:22 +0430

    diable feed generation

--- publishconf.py
+++ publishconf.py
@@ -12,8 +12,8 @@ from pelicanconf import *
 SITEURL = 'http://www.sobhe.ir/blog'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
